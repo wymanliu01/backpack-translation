7fbcfc69dd7d7893645a6c87564af5b308b13e66
Wyman Liu <wyman@digisalad.cool>
2021-07-22 10:31:40 +0800

    migration update

--- database/migrations/2021_07_21_181107_create_companies_table.php
+++ database/migrations/2021_07_21_181107_create_companies_table.php
@@ -4,21 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateImagesTable extends Migration
+class CreateCompaniesTable extends Migration
 {
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('companies', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->morphs('source');
-            $table->string('column_name')->nullable();
-            $table->string('url');
+            $table->string('name');
+            $table->text('introduction')->nullable();
             $table->timestamps();
         });
     }
 
     public function down()
     {
-        Schema::dropIfExists('images');
+        Schema::dropIfExists('companies');
     }
 }
